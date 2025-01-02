b521394fb5183bc678abca4386da5495c39c93ef
Domche <martialdomche@gmail.com>
2025-01-02 10:05:02 +0100

    kjhui

---
+++ script/apps.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
