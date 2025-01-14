2e5eb0555b3c10ffe1ee58d8ca22d9f9af30daf2
Domche <martialdomche@gmail.com>
2025-01-14 11:13:58 +0100

    kjkjkjkj

--- scripts/apps/apps.py
+++ scripts/apps/apps.py
@@ -1,29 +1,42 @@
 import streamlit as st
-import tensorflow as tf
 from PIL import Image
 import numpy as np
+from tflite_runtime.interpreter import Interpreter  # Importer TensorFlow Lite Runtime
 
-# Chemin du modèle .h5
-MODEL_PATH = '/app/Models/final_model.h5'  # Chemin absolu dans le conteneur Docker
+# Chemin du modèle TensorFlow Lite
+MODEL_PATH = '/app/Models/final_model.tflite'
 
-# Chargement du modèle Keras
-model = tf.keras.models.load_model(MODEL_PATH)
+# Chargement du modèle TensorFlow Lite
+def load_tflite_model(model_path):
+    """Charger le modèle TensorFlow Lite."""
+    interpreter = Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+    return interpreter
+
+# Préparer l'interpréteur
+interpreter = load_tflite_model(MODEL_PATH)
+
+# Obtenir les indices des entrées et sorties du modèle
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 # Fonction pour prétraiter l'image
 def preprocess_image(img):
     """Préparer l'image pour la prédiction."""
     img = img.resize((224, 224))  # Mise à l'échelle de l'image
-    img_array = np.array(img)  # Conversion en tableau numpy
+    img_array = np.array(img, dtype=np.float32)  # Conversion en tableau numpy avec dtype=float
     img_array = np.expand_dims(img_array, axis=0)  # Ajouter une dimension pour le batch
     img_array = img_array / 255.0  # Normalisation des pixels (0 à 1)
     return img_array
 
-# Fonction pour effectuer la prédiction avec le modèle Keras
-def predict_image(model, img):
+# Fonction pour effectuer la prédiction avec TensorFlow Lite
+def predict_image(interpreter, img):
     """Effectuer la prédiction sur l'image donnée."""
     img_array = preprocess_image(img)
-    predictions = model.predict(img_array)[0]  # Effectuer la prédiction
-    
+    interpreter.set_tensor(input_details[0]['index'], img_array)  # Passer les données en entrée
+    interpreter.invoke()  # Exécuter le modèle
+    predictions = interpreter.get_tensor(output_details[0]['index'])[0]  # Obtenir les résultats
+
     # Classes des maladies
     CLASS_LABELS = [
         "actinic keratosis",
@@ -35,7 +48,7 @@ def predict_image(model, img):
         "squamous cell carcinoma",
         "vascular lesion"
     ]
-    
+
     results = {CLASS_LABELS[i]: round(pred * 100, 2) for i, pred in enumerate(predictions)}
     return results
 
@@ -55,7 +68,7 @@ if uploaded_file is not None:
 
     # Faire la prédiction
     if st.button("Make Prediction"):
-        results = predict_image(model, img)
+        results = predict_image(interpreter, img)
         # Affichage des résultats
         st.write("Prediction Results:")
         for label, prob in results.items():
